1e8be2d86972a76e9b8dfef42ae23d5aa07cafcd
dineth7788 <dinethkaveeshafernando@gmail.com>
2025-08-18 20:18:22 +0530

    delete / edit item

---
+++ src/main/java/controller/DeleteCustomerServlet.java
@@ -0,0 +1,35 @@
+package controller;
+
+import util.DBUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+@WebServlet("/deleteCustomer")
+public class DeleteCustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String idStr = request.getParameter("id");
+
+        try (Connection conn = DBUtil.getConnection()) {
+            String sql = "DELETE FROM customers WHERE id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, Integer.parseInt(idStr));
+            ps.executeUpdate();
+            ps.close();
+
+            response.sendRedirect("viewCustomers.jsp");
+
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
